11f44005abfa8d1c60fd5b7d420c90cb1cec9ab0
Ricardo Branco <rbranco@suse.de>
2025-10-15 17:23:52 +0200

    DO NOT MERGE, bypass OBS

--- tests/containers/docker_cli.pm
+++ tests/containers/docker_cli.pm
@@ -33,7 +33,7 @@ sub setup {
     run_command "systemctl restart docker";
     record_info("docker info", script_output("docker info"));
 
-    run_command "docker run -d --name registry -p 5000:5000 registry.opensuse.org/opensuse/registry:2";
+    run_command "docker run -d --name registry -p 5000:5000 registry:2";
 
     # Install test dependencies
     if (is_x86_64) {
