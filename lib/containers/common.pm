6ef900f9b65b7d30a698f6e55a02609c64f1bbd8
Ricardo Branco <rbranco@suse.de>
2026-09-22 12:51:07 +0200

    containers: Remove install_packages

--- lib/containers/common.pm
+++ lib/containers/common.pm
@@ -24,7 +24,7 @@ use Utils::Architectures qw(is_aarch64);
 our @EXPORT = qw(is_unreleased_sle install_podman_when_needed install_docker_when_needed install_containerd_when_needed
   test_container_runtime test_container_image
   install_buildah_when_needed activate_containers_module check_containers_connectivity
-  switch_cgroup_version install_packages);
+  switch_cgroup_version);
 
 sub is_unreleased_sle {
     # If "SCC_URL" is set, it means we are in not-released SLE host and it points to proxy SCC url
@@ -294,19 +294,6 @@ sub switch_cgroup_version {
     select_serial_terminal;
 
     validate_script_output("cat /proc/cmdline", sub { m/systemd\.unified_cgroup_hierarchy=$setting/ });
-}
-
-sub install_packages {
-    my @pkgs = @_;
-    # skip if already installed:
-    unless (script_run("rpm -q @pkgs") == 0) {
-        if (is_transactional) {
-            trup_call("pkg install @pkgs");
-            check_reboot_changes;
-        } else {
-            zypper_call("in @pkgs");
-        }
-    }
 }
 
 1;
